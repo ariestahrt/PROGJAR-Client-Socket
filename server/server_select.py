d6aee5299154a11c3af734b37b0b74a3ac5a896d
Ariesta Heart <ariestaheart@gmail.com>
2022-03-04 22:23:54 +0700

    Finish _select
    Co-authored-by: Timotius Wirawan <timmtimm@users.noreply.github.com>

--- server/server_select.py
+++ server/server_select.py
@@ -21,60 +21,70 @@ try:
         for sock in read_ready:
             if sock == server_socket:
                 client_socket, client_address = server_socket.accept()
+                print(f"[+] New client {client_address} is connected.")
                 input_socket.append(client_socket)
             else:            	
                 data = sock.recv(BUFFER_SIZE)
-                data_split = data.decode().split(" ")
+                print(f"[!] {sock.getpeername()} << {data.decode().rstrip()}")
+
+                data_split = f"{data.decode().rstrip()} ".split(" ")
 
                 if data_split[0] == "unduh":
-                    
-
                     # check if file exist
                     filename = data.decode().split("unduh ")[1].rstrip()
                     if path.exists(f"dataset/{filename}"):
-                        sock.send(bytes("CONFIRMATION::FILE_EXIST\n", 'utf-8'))
-                        
+                        sock.send(bytes("CONFIRMATION::FILE_EXIST\n", 'utf-8'))                        
                         filesize = os.path.getsize(f"dataset/{filename}")
 
+                        # initiate headers
                         header = f"file-name: {filename}\n"
                         header += f"file-size: {filesize}\n\n\n"
                         sock.send(bytes(header, 'utf-8'))
 
                         # start sending the file
+                        total_data_sent = 0
                         with open(f"dataset/{filename}", "rb") as f:
                             while True:
                                 # read the bytes from the file
                                 bytes_read = f.read(BUFFER_SIZE)
+                                total_data_sent += len(bytes_read)
+                                #update the progress status
+                                print(f"[!] File transfer {filename} : {total_data_sent}B/{filesize}B ({int((total_data_sent/filesize)*100)}%)                 \r", end="")
+
                                 if not bytes_read:
-                                    print("Transfer fle is done!")
-                                    # sock.send(bytes("CONFIRMATION::FILE_TRANSFER_DONE\n", 'utf-8'))
+                                    print("[!] Transfer fle is done!")
                                     # file transmitting is done
                                     break
-                                # we use sendall to assure transimission in 
-                                # busy networks
+
+                                # send to client
                                 sock.sendall(bytes_read)
                     else:
                         sock.send(bytes("filenya gaada bos\n", 'utf-8'))
-
-
                 elif data_split[0] == "dataset":
                     # get file list
                     dir_list = os.listdir("dataset")
                     # Send message file list available
                     temp_msg = "List available file:"
                     for file in dir_list:
-                        temp_msg += f"\n\t[~] {file}"
+                        # check file size
+                        filesize = os.path.getsize(f"dataset/{file}")
+
+                        temp_msg += f"\n\t[~] {file} \t {filesize} B"
                     sock.send(f"\n{temp_msg}\n".encode())
+                elif data_split[0] == "REQUEST::DISCONNECT":
+                    print(f"[-] Client {sock.getpeername()} just disconnected.")
+                    sock.close()
+                    input_socket.remove(sock)
+                    None
                 else:
                     if data:
                         sock.send(data)
-                    else:                    
+                    else:
+                        print(f"[-] Client {sock.getpeername()} just disconnected.")
                         sock.close()
                         input_socket.remove(sock)
-
-                print(sock.getpeername(), data.decode())
             
 
-except KeyboardInterrupt:        
+except KeyboardInterrupt:
     server_socket.close()
     sys.exit(0)
